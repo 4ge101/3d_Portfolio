cf6539254ac9808a4b427e355625804507d5cedd
4ge101 <theforgettenone2@gmail.com>
2024-10-05 19:39:39 +0545

    completing portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/3d_Portfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
