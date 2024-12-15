e6b27d6a3d85154ac3d02d3511887dc21d5788a4
4ge101 <theforgettenone2@gmail.com>
2024-12-15 20:19:09 +0545

    updating projects and text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-800'>
-        <p>Hi, I'm Mohammad Sami Ali, a passionate web developer with a knack for creating dynamic and interactive websites. I've worked on a variety of web projects, from personal portfolios to complex applications. My expertise spans across modern technologies like HTML, CSS, JavaScript, and frameworks like React. I enjoy transforming ideas into fully functional and visually appealing websites. Let's build something amazing together!</p>
+        <p>Hi, I'm Mohammad Sami Ali, a passionate frontend web developer from Nepal and fun fact is iam 14 year old boy with some skills like HTML, CSS, JS, TS etc</p>
       </div>
 
       <div className='py-10 flex flex-col'>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,10 +17,40 @@ const Projects = () => {
       image: 'https://4ge101.github.io/School-remake-website/assets/card-img/images.jpeg',
     },
     {
-      title: 'Personal Portfolio',
-      description: 'A personal portfolio website showcasing my skills in web development and previous projects.',
+      title: 'Business Website',
+      description: 'A business website to take idea how business website looks.',
       link: 'https://4ge101.github.io/PORTFOLIO/',
       image: 'https://cdn.dribbble.com/userupload/7918389/file/original-8ed1531fd13698a0b5f6e72ef75960c2.png?resize=1024x768',
+    },
+    {
+      title: 'Netflix Clone',
+      description: 'To know how my skill is going in web devlopment i cloned netflix website',
+      link: 'https://4ge101.github.io/Netflix-Clone/',
+      image: 'https://static1.howtogeekimages.com/wordpress/wp-content/uploads/2023/08/netflix.jpg',
+    },
+    {
+      title: 'Anime Website',
+      description: 'To watching every type of anime i made this anime website',
+      link: 'https://4ge101.github.io/Anime_Website/',
+      image: 'https://themewagon.com/wp-content/uploads/2020/10/Anime.png',
+    },
+    {
+      title: 'Construction Website',
+      description: 'For who was running construction company i made for help to people contact and showing their work',
+      link: 'https://4ge101.github.io/construction_website/',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEbEUg5d-DaC8ompYdwW02pN0fsE0OHsaUwg&s',
+    },
+    {
+      title: 'UltraEdit Website',
+      description: 'To know how my skill is going in web devlopment i cloned ultra-edit website',
+      link: 'https://4ge101.github.io/construction_website/',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp8V1GNj0TMFvWaxjSmiiaJCSticAfQ5miAw&s',
+    },
+    {
+      title: 'Sarcasm Website',
+      description: 'This website i made as a first css project',
+      link: 'https://4ge101.github.io/first-advanced-website/',
+      image: 'https://pamutlabor.hu/wp-content/uploads/2018/03/plab0005162-afeaturedimage-white.jpg',
     },
   ];
 
@@ -31,7 +61,7 @@ const Projects = () => {
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-800'>
-        <p>I've worked on a variety of projects, including GHOST AI, a voice assistant that responds to commands and provides updates. I also built a 3D Island website using React and Three.js, featuring a fully interactive 3D environment. Additionally, I developed a school website with an admin panel for event management and created my personal portfolio to showcase my skills in web development.</p>
+        <p>I've worked on a variety of projects like a small AI, school website, business website, Netflix clone etc.</p>
       </div>
 
       <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 gap-8'>
